4e41ec0ec022fbad50a70b8f7390e856ab8c2709
Al Cushon <acushon@gmail.com>
2019-09-08 13:04:34 -0700

    Initial commit

---
+++ content/keys.js
@@ -0,0 +1,5 @@
+content={
+    darkskyKey: 'c0f297e9c9f293ac4005651d71160975',
+    mapboxKey: 'pk.eyJ1IjoiYWN1c2hvbiIsImEiOiJjazAyc205aXgyc3V5M21tdnVrOHB3eTI3In0.bjvVBGPY45DSHzkwI5PDRA'
+}
+module.exports=content
---
+++ content/text.js
@@ -0,0 +1,21 @@
+content={
+    index:{
+        page_title: 'Weather App',
+        title: 'Weather',
+        name: 'Al Cushon',
+        content: 'Use ths site to get your weather fool!'
+    },
+    about:{
+        page_title: 'About',
+        title: 'About Me',
+        name: 'Andrew Mead',
+        content: 'This guys is teaching me Node/Express.'
+    },
+    help:{
+        page_title: 'Help',
+        title: 'Help',
+        name: 'Andrew Mead',
+        content: 'This is some helpful text.'
+    }
+}
+module.exports=content
